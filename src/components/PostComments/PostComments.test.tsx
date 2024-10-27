29ce0816d3306a784803b4c01d1c7ab448987a02
Vinícius Santos <vinisantoosk@gmail.com>
2024-10-27 16:41:31 -0300

    feat: testando a inserção de dois comentarios por data-testid

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,20 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    it('Deve permitir a inserção de dois comentários', () => {
+        render(<PostComment />);
+        
+        const textarea = screen.getByTestId('comment-input-area');
+        const button = screen.getByTestId('btn-comment-submit');
+                
+        fireEvent.change(textarea, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(button);
+        expect(screen.getAllByTestId('comment-item')[0]).toHaveTextContent('Primeiro comentário');
+        
+        fireEvent.change(textarea, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(button);
+        
+        expect(screen.getAllByTestId('comment-item')[1]).toHaveTextContent('Segundo comentário');
+        
+    });
 });
